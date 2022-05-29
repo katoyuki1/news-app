e9cdc7498eebaa71d27bba8e771d11ba73425616
yuuki.kato <0me3210g039530z@gmail.com>
2022-05-29 18:20:52 +0900

    first commit Section 4

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +8,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row"
+  },
+  leftContainer: {
+    width: 100
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray",
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{uri: 'https://picsum.photos/200/300'}}
+        />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3} style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          </Text>
+          <Text style={styles.subText}>
+            React News by y-kato
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
