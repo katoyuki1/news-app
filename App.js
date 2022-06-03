df84f5641a6f380c84fc58dec53b4ceea8230b82
yuuki.kato <0me3210g039530z@gmail.com>
2022-06-04 08:57:13 +0900

    add debugger

--- App.js
+++ App.js
@@ -1,56 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    borderColor: "gray",
-    borderWidth: 1,
-    flexDirection: "row"
-  },
-  leftContainer: {
-    width: 100
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between",
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray",
-  }
-});
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-        <Image
-          style={{width: 100, height: 100}}
-          source={{uri: 'https://picsum.photos/200/300'}}
-        />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-          </Text>
-          <Text style={styles.subText}>
-            React News by y-kato
-          </Text>
-        </View>
-      </View>
-    </View>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
